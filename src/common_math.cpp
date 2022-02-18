8f282eed44eab2a90ce7a19c63088082aaa424de
abkour <floralysman@gmail.com>
2022-02-18 15:13:23 +0000

    Replaced hard-coded PI constant with constant in common_helper.hpp

--- src/common_math.cpp
+++ src/common_math.cpp
@@ -4,10 +4,8 @@
 
 namespace specter {
 
-constexpr float PI = 3.1415926535;
-
 float radians(const float angleInDegrees) {
-	return angleInDegrees * PI / 180.f;
+	return angleInDegrees * Pi / 180.f;
 }
 
 float log8(const float x) {
